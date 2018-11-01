21001ec0cd5f231d6f068f8c5adb4a4e0faec18d
Christopher Johnson <Jens@Christophers-MacBook-Pro-2.local>
2018-11-01 16:19:57 -0700

    L0 objectives met

---
+++ frontend/js/nightlife.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(function() {
+        $('input[name="datetimes"]').daterangepicker({
+            timePicker: true,
+            startDate: moment().startOf('hour'),
+            endDate: moment().startOf('hour').add(32, 'hour'),
+            locale: {
+                format: 'M/DD hh:mm A'
+            }
+        });
+    });
+});
