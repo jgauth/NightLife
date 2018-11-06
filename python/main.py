7b813bb9ec9b031bd5cef9e5f516e338bf51d3b1
john <john.gauthier5@gmail.com>
2018-11-06 13:45:01 -0800

    started sqlparser

--- python/main.py
+++ python/main.py
@@ -7,24 +7,26 @@ import os
 from flask import Flask, request, redirect, url_for, flash, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlparser import SqlParser
+from utils import *
 
 app = Flask("john")
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db'
-app.secret_key = 'ASDGBJQWVWEC@$%#%&#WERGAJBEF'
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db'
+# app.secret_key = 'ASDGBJQWVWEC@$%#%&#WERGAJBEF'
 
-db = db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# db = db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
-conn_str = "dbname='%s' user='%s' host='%s' password='%s'" % (
-    'john', 'john', 'db', 'johnpassword')
-# use our connection values to establish a connection
-conn = psycopg2.connect(conn_str)
+# conn_str = "dbname='%s' user='%s' host='%s' password='%s'" % (
+#     'john', 'john', 'db', 'johnpassword')
+# # use our connection values to establish a connection
+# conn = psycopg2.connect(conn_str)
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    location = db.Column(Geometry('LINESTRING'))
+# class Event(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(128))
+#     location = db.Column(Geometry('LINESTRING'))
 
 @app.route("/api/healthcheck", methods=['GET'])
 def healthcheck():
@@ -48,3 +50,16 @@ def create_event():
     response = jsonify({ 'message': 'upload success' })
     response.status_code = 200
     return response
+
+parser = SqlParser()
+parser.init()
+# parser.cursor.execute("DROP TABLE IF EXISTS event")
+
+
+parser.cursor.execute('CREATE TABLE IF NOT EXISTS event (id SERIAL PRIMARY KEY, geom GEOMETRY(Point), name VARCHAR(128))')
+parser.cursor.execute("INSERT INTO event (geom, name) VALUES (ST_GeomFromText('POINT(56.1818 -41.18181)'), 'test123')")
+parser.cursor.execute('SELECT name, ST_AsGeoJSON(geom) FROM event')
+for item in parser.cursor:
+    eprint(type(item))
+    eprint(item)
+parser.connection.commit()
--- python/sqlparser.py
+++ python/sqlparser.py
@@ -7,39 +7,75 @@ LOG_TIME = datetime.datetime.now
 
 # Our database credentials
 
-HOST = "nightlife-db.crtru8tqdhdu.us-west-2.rds.amazonaws.com" 
-PORT = "5432" 
-USER = "CIS422"
-DBNAME = "nightlifeDBd"
-PASSWORD = "Bnh8wMiz3J9Fpz9!j#*N16lgq8NDf%K#"
+# HOST = "nightlife-db.crtru8tqdhdu.us-west-2.rds.amazonaws.com" 
+# PORT = "5432" 
+# USER = "CIS422"
+# DBNAME = "nightlifeDBd"
+# PASSWORD = "Bnh8wMiz3J9Fpz9!j#*N16lgq8NDf%K#"
 CONNECT_TIMEOUT = "5"
+
+HOST = "db" 
+PORT = "5432" 
+USER = "john"
+DBNAME = "john"
+PASSWORD = "johnpassword"
+
+conn_str = "dbname='%s' user='%s' host='%s' password='%s'" % (
+    DBNAME, USER, HOST, PASSWORD, CONNECT_TIMEOUT)
 
 
 class SqlParser:
     def __init__(self):
-        self._connection = None
+        self.connection = None
+        self.cursor = None
     def init(self):
         '''
         Set up logs, initialize database connection
         '''
-        logging.basicConfig(filename=NIGHTLIFE_LOG, level=logging.INFO)
-        logging.basicConfig(filename=NIGHTLIFE_LOG, level=logging.ERROR)
-        logging.info(str(LOG_TIME())+": SQL Parser session initialized")
-        logging.error(str(LOG_TIME())+": SQL Parser session initialized")
+        # logging.basicConfig(filename=NIGHTLIFE_LOG, level=logging.DEBUG, format='%(levelname)s:%(asctime)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        # logging.info("SQL Parser session initialized")
+        # logging.error("SQL Parser session initialized")
 
         try:
-            connection = psycopg2.connect(dbname=DBNAME, user=USER, host=HOST, password=PASSWORD, connect_timeout=CONNECT_TIMEOUT)
-            self._connection = connection
-            self._cursor = self._connection.cursor()
-            logging.info(str(LOG_TIME())+": Established database connection")
+            self.connection = psycopg2.connect(conn_str)
+            self.cursor = self.connection.cursor()
+            # logging.info("Established database connection")
             return True
         except (Exception, NameError, psycopg2.DatabaseError) as error:
-            logging.error(str(LOG_TIME())+": Connection to database failed: %s", error)
+            # logging.error("Connection to database failed: %s", error)
             return False
-        
-        
 
+    def reset_table(self):
+        self.cursor.execute("DROP TABLE IF EXISTS events")
+        self.cursor.execute("""
+        CREATE TABLE events (
+            id SERIAL PRIMARY KEY, 
+            location GEOMETRY(Point) NOT NULL, 
+            address VARCHAR(256) NOT NULL,
+            name VARCHAR(64) NOT NULL,
+            host VARCHAR(64) NOT NULL,
+            time_start TIMESTAMP NOT NULL,
+            time_end TIMESTAMP NOT NULL,
+            theme VARCHAR(64),
+            description VARCHAR(256)
+            )""")
+        self.connection.commit()
 
-if __name__ == "__main__":
-    parser = SqlParser()
-    parser.init()
+    def insert_event(self, lat, long, address, name, host, time_start, time_end, theme='NULL', description='NULL'):
+        """
+        lat, long - float
+        address, name, host, time_start, time_end, theme, description - string 
+        """
+        self.cursor.execute("""
+        INSERT INTO events (location, address, name, host, time_start, time_end, theme, description) VALUES (
+            ST_GeomFromText('POINT({} {})'), #lat/long
+            '{}', #address
+            '{}', #name
+            '{}', #host
+            '{}', #time_start
+            '{}', #time_end
+            '{}', #theme
+            '{}', #description
+        )""".format(lat, long, address, name, host, time_start, time_end, theme='NULL', description='NULL'))
+
+    def get_events_json(self):
