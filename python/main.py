4df1a7bdd37c06ff77364ba0aca7fab6aecfb67b
john <john.gauthier5@gmail.com>
2018-11-10 15:43:19 -0800

    moved geocode to utils

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 import json, os, requests
-from utils import *
+from utils import eprint, geocode
 from flask import Flask, request, redirect, url_for, flash, abort, jsonify
 from models import db, Event
 from forms import NewEventForm, TestForm
@@ -19,34 +19,6 @@ GMAPS_KEY = "AIzaSyDK0VvGmW9rc1y-oX6APE7Rc3wQnoRUI58"
 
 db.app = app
 db.init_app(app)
-
-def geocode(address):
-    '''
-    Takes address(string)
-    returns tuple of lat(float), lng(float), formatted_address(string)
-    note: formatted_address will remove apt #s, room #, etc'''
-
-    # example
-    # https://maps.googleapis.com/maps/api/geocode/json?address=1337+Hilyard+St,+Eugene,+OR+97401&key=AIzaSyDK0VvGmW9rc1y-oX6APE7Rc3wQnoRUI58
-
-    # Format address to be url valid
-    # may need to adjust formatting to properly handle front-end format
-    address = address.replace(' ', '+').replace('.', '')
-
-    parameters = {'address':address, 'key':GMAPS_KEY}
-    try:
-        response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", parameters).json()
-
-        lat = response['results'][0]['geometry']['location']['lat']
-        lng = response['results'][0]['geometry']['location']['lng']
-        formatted_address = response['results'][0]['formatted_address']
-
-        return (lat, lng, formatted_address)
-
-    except requests.exceptions.RequestException as e:
-        # requests error
-        # do something
-        pass
 
 
 @app.route("/api/healthcheck", methods=['GET'])
--- python/utils.py
+++ python/utils.py
@@ -2,3 +2,31 @@ import sys
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+def geocode(address):
+    '''
+    Takes address(string)
+    returns tuple of lat(float), lng(float), formatted_address(string)
+    note: formatted_address will remove apt #s, room #, etc'''
+
+    # example
+    # https://maps.googleapis.com/maps/api/geocode/json?address=1337+Hilyard+St,+Eugene,+OR+97401&key=AIzaSyDK0VvGmW9rc1y-oX6APE7Rc3wQnoRUI58
+
+    # Format address to be url valid
+    # may need to adjust formatting to properly handle front-end format
+    address = address.replace(' ', '+').replace('.', '')
+
+    parameters = {'address':address, 'key':GMAPS_KEY}
+    try:
+        response = requests.get("https://maps.googleapis.com/maps/api/geocode/json", parameters).json()
+
+        lat = response['results'][0]['geometry']['location']['lat']
+        lng = response['results'][0]['geometry']['location']['lng']
+        formatted_address = response['results'][0]['formatted_address']
+
+        return (lat, lng, formatted_address)
+
+    except requests.exceptions.RequestException as e:
+        # requests error
+        # do something
+        pass
