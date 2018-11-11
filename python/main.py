e2747d16618cdacd1a32c576c7b5117d863e3464
john <john.gauthier5@gmail.com>
2018-11-11 12:34:16 -0800

    added generation of test events, get_all api endpoint to return json of all events

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
-import json, os, requests
-from utils import eprint, geocode
+import json, os
+from utils import *
 from flask import Flask, request, redirect, url_for, flash, abort, jsonify
 from models import db, Event
 from forms import NewEventForm, TestForm
@@ -15,8 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024
 app.secret_key = b'\xc48\xd3!w\xf8U\x06\xc9([\xac\xd9\xfe\xbaj'
 
-GMAPS_KEY = "AIzaSyDK0VvGmW9rc1y-oX6APE7Rc3wQnoRUI58"
-
 db.app = app
 db.init_app(app)
 
@@ -25,6 +23,38 @@ db.init_app(app)
 def healthcheck():
     return jsonify({ 'data': 'sup' })
 
+@app.route("/api/event/gen_events/<int:n>")
+def gen_events(n):
+    event_list = generate_test_events(n)
+    for e in event_list:
+        db.session.add(e)
+    db.session.commit()
+    response = jsonify({ 'message': 'success' })
+    response.status_code = 200
+    return response
+
+
+@app.route("/api/event/get_all", methods=['GET'])
+def get_all():
+    all_events = Event.query.all()
+    json_list = []
+    for row in all_events:
+        item = {}
+        item['id'] = row.id
+        item['name'] = row.name
+        item['lat'] = row.lat
+        item['lng'] = row.lng
+        item['address'] = row.address
+        item['host'] = row.host
+        item['theme'] = row.theme
+        item['description'] = row.description
+        item['time_start'] = row.time_start
+        item['time_end'] = row.time_end
+        json_list.append(item)
+    response = jsonify({'events':json_list})
+    response.status_code = 200
+    return response
+
 @app.route("/api/event/create", methods=['POST','PUT'])
 def create_event():
 
@@ -32,9 +62,19 @@ def create_event():
 
     eprint("method: " + request.method)
     if request.method == 'POST' or request.method == 'PUT':
+        # eprint(request.form)
+        street = request.form['street']
+        city = request.form['city']
+        state = request.form['state']
+        zip = request.form['zip']
+
+        address = "{}, {}, {} {}".format(street, city, state, zip)
+        eprint(address)
+        geo_tuple = geocode(address)
+        eprint(geo_tuple)
         if form.validate(): # WTForm validation
-            eprint(request.form['name'])
-            eprint(request.form['address'])
+            
+            # eprint(request.form['address'])
 
             #handle db here
             ############# ffffffffinish this
@@ -42,11 +82,11 @@ def create_event():
             address = request.form['address']
             loc_data = geocode(address)
             lat = loc_data[0]
-            long = loc_data[1]
-            new_event = Event(name=name, geo='POINT({} {})'.format(lat, long), lat=lat, long=long, address=address)
+            lng = loc_data[1]
+            new_event = Event(name=name, geo='POINT({} {})'.format(lat, lng), lat=lat, lng=lng, address=address)
 
-            db.session.add(new_event)
-            db.session.commit()
+            # db.session.add(new_event)
+            # db.session.commit()
 
             response = jsonify({ 'message': 'validation/upload success' })
             response.status_code = 200
--- python/models.py
+++ python/models.py
@@ -8,7 +8,7 @@ class Event(db.Model):
     name = db.Column(db.String(255))        #Name of Event
     geo = db.Column(Geometry('POINT'))      #PostGIS Geometry
     lat = db.Column(db.Float)               #Latitude
-    long = db.Column(db.Float)              #Longitude
+    lng = db.Column(db.Float)               #Longitude
     address = db.Column(db.String(255))     #Physical address (eg. 123 Nowhere lane...)
     host = db.Column(db.String(255))        #Event host
     theme = db.Column(db.String(255))       #Event theme
