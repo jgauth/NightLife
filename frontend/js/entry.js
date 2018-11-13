12cce8564201f4fca90b9f59fdbc3d9f7f11d7ba
Christopher Johnson <Jens@Christophers-MacBook-Pro-2.local>
2018-11-13 13:16:50 -0800

    more updates

--- frontend/js/entry.js
+++ frontend/js/entry.js
@@ -1,5 +1,7 @@
 let hosts = ["Alpha Epsilon Pi", "Alpha Sigma Phi", "Alpha Tau Omega", "Chi Psi", "Delta Sigma Phi", 
-"Delta Tau Delta", "Delta Upsilon", "Kappa Sigma", "Lambda Chi Alpha"
+"Delta Tau Delta", "Delta Upsilon", "Kappa Sigma", "Lambda Chi Alpha", "Phi Gamma Delta (FIJI)",
+"Pi Kappa Alpha", "Pi Kappa Phi", "Sigma Alpha Epsilon", "Sigma Alpha Mu", "Sigma Chi", "Sigma Nu",
+"Sigma Phi Epsilon", "Theta Chi"
 ];
 
 $(document).ready(function(){
