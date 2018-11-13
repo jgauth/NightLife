5e56bc880b6e957e4bd725746ca822d644e332fc
Christopher Johnson <Jens@Christophers-MacBook-Pro-2.local>
2018-11-13 13:50:00 -0800

    fixed autocomplete

--- frontend/js/entry.js
+++ frontend/js/entry.js
@@ -1,22 +1,10 @@
 let hosts = ["Alpha Epsilon Pi", "Alpha Sigma Phi", "Alpha Tau Omega", "Chi Psi", "Delta Sigma Phi", 
 "Delta Tau Delta", "Delta Upsilon", "Kappa Sigma", "Lambda Chi Alpha", "Phi Gamma Delta (FIJI)",
 "Pi Kappa Alpha", "Pi Kappa Phi", "Sigma Alpha Epsilon", "Sigma Alpha Mu", "Sigma Chi", "Sigma Nu",
-"Sigma Phi Epsilon", "Theta Chi"
-];
+"Sigma Phi Epsilon", "Theta Chi"];
 
 $(document).ready(function(){
-    $('input#hostName').autocomplete({
+    $('input#eventHostInput').autocomplete({
         source: hosts
-    });
-
-    $(function() {
-        $('input[name="datetimes"]').daterangepicker({
-            timePicker: true,
-            startDate: moment().startOf('hour'),
-            endDate: moment().startOf('hour').add(32, 'hour'),
-            locale: {
-                format: 'M/DD hh:mm A'
-            }
-        });
     });
 });
